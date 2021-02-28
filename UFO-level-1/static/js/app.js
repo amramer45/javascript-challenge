7af731ea6c011269613fcc0dbd587249f027cd1f
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-02-28 13:09:18 -0500

    exceptions

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 console.log(tableData);
 
 // Add column names
-var columns = ["date/time", "city", "state", "country", "shape", "duration", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments"]
 
 // Get a reference to the table body 
 var tbody = d3.select("tbody");
@@ -25,19 +25,15 @@ var button = d3.select("#filter-btn");
 button.on("click", function () {
     tbody.html("");
 
-    var inputElement = d3.select("#date/time");
+    var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
     console.log(`The total number of UFO sightings on ${inputValue} is: ${filteredData.length}`);
 
-    filteredData.forEach(function(selections) {
-        console.log(selections);
-        var row = tbody.append("tr");
-        Object.entries(selections).forEach(function([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
+    if (filteredData.length != 0) {
+        tableData(filteredData);
+    } else {
+        tbody.append("tr").append("td").text("No data found for this date.");
+    }
 });
