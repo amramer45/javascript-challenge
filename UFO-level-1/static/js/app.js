a4c4c3fabaf0e4e0c446b4fce72f636afeea829a
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-02-28 11:30:58 -0500

    more updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,9 +3,41 @@ var tableData = data;
 console.log(tableData);
 
 // Add column names
-var columns = ["date/time", "city", "state", "country", "shape", "comment"]
+var columns = ["date/time", "city", "state", "country", "shape", "duration", "comments"]
 
 // Get a reference to the table body 
 var tbody = d3.select("tbody");
 
 // Values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Create the button
+var button = d3.select("#filter-btn");
+button.on("click", function () {
+    tbody.html("");
+
+    var inputElement = d3.select("#date/time");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(`The total number of UFO sightings on ${inputValue} is: ${filteredData.length}`);
+
+    filteredData.forEach(function(selections) {
+        console.log(selections);
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
