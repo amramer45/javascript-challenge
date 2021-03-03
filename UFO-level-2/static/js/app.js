4d0d95637709208633edad78120d942873a87c74
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-02 19:04:25 -0500

    filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,16 +1,17 @@
-// from data.js
+// import data from data.js
 var tableData = data;
 //console.log(tableData);
 
 // Add column names
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// Get a reference to the table body 
+// Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Values for each column
 var loadData = (startData) => {
     startData.forEach(function (ufoSighting) {
+        //console.log(ufoSighting);
         var row = tbody.append("tr");
         columns.forEach(function(column) {
             row.append("td").text(ufoSighting[column])
@@ -53,41 +54,35 @@ button.on("click", function () {
                                                     sighting.state === inputStateValue ||
                                                     sighting.country === inputCountryValue ||
                                                     sighting.shape === inputShapeValue);
+    
+    console.log(filteredData);
+                                                    
+    // Apply filter, collect data to keep rows that return criteria
+    if (inputDateValue !== "" ) {
+        filteredData = filteredData.filter((ufoSightings) => ufoSightings.datetime === inputDateValue);
+    }
 
-    // console.log filter values  
+    if (inputCityValue !== "" ) {
+        filteredData = filteredData.filter((ufoSightings) => ufoSightings.cityName === inputCityValue);
+    }
+
+    if (inputStateValue !== "" ) {
+        filteredData = filteredData.filter((ufoSightings) => ufoSightings.stateName === inputStateValue);
+    }
+
+    if (inputCountryValue !== "" ) {
+        filteredData = filteredData.filter((ufoSightings) => ufoSightings.countryName === inputStateValue);
+    }
+
+    if (inputShapeValue !== "" ) {
+        filteredData = filteredData.filter((ufoSightings) => ufoSightings.shapeType === inputShapeValue);
+    }
+
     console.log(filteredData);
 
-    // var filteredData = tableData;
-    // loadData(filteredData);
-    // console.log(filteredData);
-
-    // // Apply filter, collect data to keep rows that return criteria
-    // if (inputDateValue !== "" ) {
-    //     filteredData = filteredData.filter((ufoSightings) => ufoSightings.datetime === inputDateValue);
-    // }
-
-    // if (inputCityValue !== "" ) {
-    //     filteredData = filteredData.filter((ufoSightings) => ufoSightings.cityName === inputCityValue);
-    // }
-
-    // if (inputStateValue !== "" ) {
-    //     filteredData = filteredData.filter((ufoSightings) => ufoSightings.stateName === inputStateValue);
-    // }
-
-    // if (inputCountryValue !== "" ) {
-    //     filteredData = filteredData.filter((ufoSightings) => ufoSightings.countryName === inputStateValue);
-    // }
-
-    // if (inputShapeValue !== "" ) {
-    //     filteredData = filteredData.filter((ufoSightings) => ufoSightings.shapeType === inputShapeValue);
-    // }
-
-    // console.log(filteredData);
-
-    //Show in console ufo sightings
+    //Show in console date tried and number of sightings
     console.log(`The total number of UFO sightings is: ${filteredData.length}`);
-
-    // If function
+    // If functuon to help error processsing and give info to user
     if (filteredData.length != 0) {
         loadData(filteredData);
     } else {
@@ -105,4 +100,4 @@ buttonReset.on("click", function () {
     loadData(tableData);
     // Show in console number of ufo sightings
     console.log(`The total number of sightings currently in the database: ${data.length}`);
-})
+});
